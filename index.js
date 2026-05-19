d4f555b9ed9cb0258e59b53d59b4fb22eb1c0377
RyUk346 <mahmudur346@gmail.com>
2026-05-20 01:21:37 +0600

    Add tutors API routes

--- index.js
+++ index.js
@@ -24,16 +24,75 @@ const client = new MongoClient(uri, {
   },
 });
 
-let tutorCollection;
-let bookingCollection;
-
 async function run() {
   try {
     await client.connect();
 
     const db = client.db("tutor-booking-db");
-    tutorCollection = db.collection("tutors");
-    bookingCollection = db.collection("bookings");
+    const tutorCollection = db.collection("tutors");
+    const bookingCollection = db.collection("bookings");
+
+    app.get("/featured-tutors", async (req, res) => {
+      const result = await tutorCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/tutors", async (req, res) => {
+      const { search = "", startDate, endDate } = req.query;
+
+      const query = {};
+
+      if (search) {
+        query.tutorName = { $regex: search, $options: "i" };
+      }
+
+      if (startDate || endDate) {
+        query.sessionStartDate = {};
+
+        if (startDate) {
+          query.sessionStartDate.$gte = startDate;
+        }
+
+        if (endDate) {
+          query.sessionStartDate.$lte = endDate;
+        }
+      }
+
+      const result = await tutorCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/tutors/:tutorsId", async (req, res) => {
+      const { tutorsId } = req.params;
+      const query = { _id: new ObjectId(tutorsId) };
+      const result = await tutorCollection.findOne(query);
+
+      res.send(result);
+    });
+
+    app.post("/tutors", async (req, res) => {
+      const tutorData = req.body;
+
+      const tutor = {
+        ...tutorData,
+        hourlyFee: Number(tutorData.hourlyFee),
+        totalSlot: Number(tutorData.totalSlot),
+        createdAt: new Date().toISOString(),
+      };
+
+      const result = await tutorCollection.insertOne(tutor);
+
+      res.send(result);
+    });
 
     console.log("MongoDB connected successfully");
   } catch (error) {
